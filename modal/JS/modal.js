45287dea57b178ae938d0b3c801d7e11e2bff86a
jwkim409 <122677237+jwkim409@users.noreply.github.com>
2023-01-29 21:48:30 +0900

    Add files via upload

---
+++ modal/JS/modal.js
@@ -0,0 +1,8 @@
+const open = document.querySelector("#open");
+const modalBox = document.querySelector("#modal-box");
+const close = document.querySelector("#close");
+
+open.addEventListener("click", () => {
+  modalBox.classList.toggle("active");
+}
+);
